9378c4f275bb4ef10de8eaf224f2998654246f4a
norlinlara <nslara@uth.hn>
2024-11-23 13:28:07 -0600

    ExamenParcialII

---
+++ src/main/java/com/mycompany/gestionmedicanl/GestionMedicaNL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionmedicanl;
+
+/**
+ *
+ * @author hi
+ */
+public class GestionMedicaNL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
